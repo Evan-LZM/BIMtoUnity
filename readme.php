21850505998058952bf4578edd57db946dfe3902
Evan-LZM <zliu246@aucklanduni.ac.nz>
2019-06-25 13:10:08 +1200

    add data

---
+++ readme.php
@@ -0,0 +1,21 @@
+<?php
+    $currentPath=__DIR__.'\woodsideData';
+    $fileresult=scandir($currentPath);
+    unset($fileresult[0]);
+    unset($fileresult[1]);
+    $fileresult = array_values($fileresult);
+    $filecount=count($fileresult);
+
+    for($i=0;$i<$filecount;$i++){
+        $path=$currentPath.'\\'.$fileresult[$i];
+        $myFile=fopen($path,"r") or die ("Could not open file");
+        $str=fread($myFile, filesize($path));
+        echo "?TableName:".$fileresult[$i].$str;
+        fclose($myFile);
+    }
+
+    // $filepath=$currentPath.'\data'.'\53 - 57 Woodside Ave (2019)_Assemblies.txt';
+    // $myFile=fopen($filepath,"r") or die ("Could not open file");
+    // echo fread($myFile, filesize($filepath));
+    // fclose($myFile);
+?>
